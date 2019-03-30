68196c31e9463eaacec5aae2e362a8935be81bbc
sickbear <sickbear@mail.ru>
2019-03-30 11:09:58 +0300

    added scripts & fonts, finished gulpfile settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,120 @@
 'use strict';
  
 const gulp = require('gulp'),
+      concat = require('gulp-concat'),
+      browserSync = require("browser-sync").create(),
       sass = require('gulp-sass'),
       importCss = require('gulp-import-css'),
-      image = require('gulp-image');
+      cleanCss = require('gulp-clean-css'),
+      prefixer = require('gulp-autoprefixer'),
+      uglify = require('gulp-uglify'),
+      image = require('gulp-image'),
+      del = require('del'),
+      reload = browserSync.reload;
 
-gulp.task('html', function() {
-    return (
-        gulp.src('./src/*.html')
-            .pipe(gulp.dest('./dist'))
-    )
-})
+const path = {
+    build: {
+        html: 'dist/',
+        js: 'dist/js/',
+        css: 'dist/css/',
+        img: 'dist/images/',
+        fonts: 'dist/fonts/'
+    },
+    src: {
+        html: 'src/*.html',
+        js: 'src/js/*.js',
+        style: 'src/styles/style.scss',
+        img: 'src/images/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: {
+        html: 'src/*.html',
+        js: 'src/js/*.js',
+        style: 'src/styles/style.scss',
+        img: 'src/images/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './dist'
+}
 
-gulp.task('sass', function() {
-    gulp.src('./src/sass/**/*.scss')
+function html() {
+    return gulp.src(path.src.html)
+            .pipe(gulp.dest(path.build.html))
+            .pipe(reload({stream: true}));
+}
+
+function js() {
+    return gulp.src(path.src.js)
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+        .pipe(reload({stream: true}));
+}
+
+function style() {
+    return gulp.src(path.src.style)
         .pipe(importCss())
+        .pipe(cleanCss({
+            compatibility: 'ie8'
+        }))
+        .pipe(prefixer({
+            browsers: ['> 0.1%'],
+            cascade: false
+        }))
         .pipe(sass({
             outputStyle: 'compressed'
         }))
-        .pipe(gulp.dest('./dist/css'))
-});
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({stream: true}));
+}
 
-gulp.task('image', function() {
-    gulp.src('./src/images/*')
+function images() {
+    return gulp.src(path.src.img)
         .pipe(image())
-        .pipe(gulp.dest('./dist/images'));
-});
+        .pipe(gulp.dest(path.build.img))
+        .pipe(reload({stream: true}));
+}
 
-gulp.task('default', gulp.series('html', 'image'));
+function fonts() {
+    return gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts));
+}
 
-gulp.task('watch', function() {
-    gulp.watch('./src/sass/**/*.scss', gulp.series('sass'));
-});
+function clean() {
+    return del(['dist/*']);
+}
+
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+    
+    gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.js, js);
+    gulp.watch(path.watch.style, style);
+    gulp.watch(path.watch.img, images);
+    gulp.watch(path.watch.fonts, fonts);
+}
+
+gulp.task('html', html);
+gulp.task('js', js);
+gulp.task('style', style);
+gulp.task('images', images);
+gulp.task('fonts', fonts);
+gulp.task('watch', watch);
+
+gulp.task(
+    'build', 
+    gulp.series(clean,
+        gulp.parallel(
+            html,
+            js,
+            style,
+            images,
+            fonts
+        )
+    )
+)
+
